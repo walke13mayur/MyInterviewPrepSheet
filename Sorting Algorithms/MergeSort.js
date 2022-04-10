e4d496be461a80de53120e6ee47fa03bce1facca
Mayur Walke <MayurWalke13@gmail.com>
2022-04-10 18:23:56 +0530

    commiting sorting algorithms

---
+++ Sorting Algorithms/MergeSort.js
@@ -0,0 +1,76 @@
+// need to study once again
+
+
+// Merges two already sorted arrays
+function merge(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
+    }
+    while(i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
+}
+//  var m =merge([100,200], [1,2,3,5,6])
+// console.log(m)
+
+
+
+
+
+
+
+
+
+
+// Merge function from earlier
+function merge(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
+    }
+    while(i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
+}
+
+// Recrusive Merge Sort
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, sright);
+}
+
+
+  var ms= mergeSort([10,24,76,73]);
+  console.log(Ms);
